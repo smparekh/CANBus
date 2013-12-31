b58eed1fdceb5a1ef644ccca084a75a3a98a805b
David <davsuss@vt.edu>
2013-12-31 16:12:27 -0500

    Added API Mode 1 support for Xbee Antennas

--- development/FSAE_CANBus/UART.c
+++ development/FSAE_CANBus/UART.c
@@ -2,6 +2,78 @@
 #include "UART.h"
 #include "HardwareProfile.h"
 
+void SendZigbeeMessage(UINT8* message, UINT8 length)
+{
+ Zigbee msg;
+
+ //Should not change......
+ msg.Delimiter = 0x7E;
+ ///////////////////////
+
+ int lth = 5 + length;
+
+ msg.OPTIONS = 0x00;
+ msg.LENGTH_MSB = ((lth & 0x0FF00) >> 8);
+ msg.LENGTH_LSB = (lth & 0x0FF);
+ msg.API_ID = 0x01;
+ msg.FRAMEID = 0x01;
+ msg.DEST_MSB = 0x50;
+ msg.DEST_LSB = 0x01;
+ UINT8 * payload[length];
+ int i = 0;
+ for(i = 0; i < length; i++)
+ {
+     msg.DATA[i] = message[i];
+ }
+ msg.CHECKSUM = CalculateChecksum(&msg,length);
+ UINT8 serialMessage[length + 9];SerializeMessage(&msg,length,serialMessage);
+
+ for(i = 0; i < (length + 9); i++)
+ {
+     PutCharacter((char)serialMessage[i]);
+ }
+
+
+}
+UINT8 CalculateChecksum(Zigbee* msg, int payloadLength)
+{
+    UINT8 Check = 0x00;
+
+    Check += msg->API_ID;
+    Check += msg->FRAMEID;
+    Check += msg->DEST_MSB;
+    Check += msg->DEST_LSB;
+    Check += msg->OPTIONS;
+    int i = 0;
+    for(i = 0; i < (payloadLength); i++)
+    {
+        Check += msg->DATA[i];
+    }
+
+   return (0xFF - (Check & 0x0FF));
+
+}
+void SerializeMessage(Zigbee* msg,UINT8 length, UINT8 * result)
+{
+
+    int i = 0;
+    result[i++] = msg->Delimiter;
+    result[i++] = msg->LENGTH_MSB;
+    result[i++] = msg->LENGTH_LSB;
+    result[i++] = msg->API_ID;
+    result[i++] = msg->FRAMEID;
+    result[i++] = msg->DEST_MSB;
+    result[i++] = msg->DEST_LSB;
+    result[i++] = msg->OPTIONS;
+    int x = 0;
+    for(x = 0; x < (length); x++)
+    {
+        result[i++] = msg->DATA[x];
+    }
+    result[i++] = msg->CHECKSUM;
+
+
+}
 
 void initUART()
 {
@@ -47,10 +119,9 @@ void __ISR(_UART1_VECTOR, ipl2) UART_int_handler(void) {
         INTClearFlag(INT_SOURCE_UART_RX(UART1));
 
         // Do Something with the data
-
         // Toggle LED to indicate UART activity
         mPORTGToggleBits(BIT_15);
-        WriteString("21");
+        
 
     }
 
